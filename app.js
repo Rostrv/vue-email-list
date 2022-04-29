0bb78eccc702f24c3135bb75ec4b6931413d83b2
Rostrv <trovatorosario91@gmail.com>
2022-04-29 15:54:58 +0200

    first and last commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        mail_random: '',
+        mailList: []
+    },
+    methods: {},
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {
+                    this.mail_random = response.data.response
+                    this.mailList.push(this.mail_random)
+                })
+
+
+        }
+    }
+
+
+})
